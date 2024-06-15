db39c8270fbcbc72298ab61b86bf47e568bbaff4
Your Name <you@example.com>
2024-06-15 13:49:50 +0200

    deploy

--- src/Star.js
+++ src/Star.js
@@ -2,7 +2,7 @@ import React from 'react';
 import DataContext from "./context/DataContext"
 import { useContext } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 
 
